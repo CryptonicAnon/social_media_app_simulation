92ea8ccb343452100a429faf2c6a08192051d075
dwayne <dwayne.reid@us.af.mil>
2024-10-29 09:36:32 -0500

    main application file. Currently able to toggle between pages

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 #from command line run 'python3 -m flask run' then access http://127.0.0.1:5000
 #will have to create individual format files to design layout for endpoint (or could realistically make em all look the same)
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template('home.html')
 
 @app.route("/inbox")
 def inbox():
-    return "This is your inbox"
+    return render_template('inbox.html')
 
-@app.route("/news_feed")
+@app.route("/notifications")
 def newsfeed():
-    return "This is your news feed"
+    return render_template('notifications.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
