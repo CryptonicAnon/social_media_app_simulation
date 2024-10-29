119ab0cbae77b1e7b0e4b78e530802a025482993
Christian Cipolloni <ccipolloni12255@gmail.com>
2024-10-29 09:55:37 -0500

    Update models.py

---
+++ models.py
@@ -0,0 +1,14 @@
+import datetime
+
+from flask_bcrypt import generate_password_hash
+from flask_login import UserMixin
+from peewee import *
+
+DATABASE = SqliteDatabase('social.db')
+
+class User(UserMixin, Model):
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
+    joined_at = DateTimeField(default=datetime.datetime.now)
+    is_admin = BooleanField(default=False)
